fccb97e66b5b926da184d94040b319de0bd54f46
Santhosh Devadasan <santhosh.devadasan@in.ibm.com>
2020-12-07 16:04:53 -0600

    Added Home screen and product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
-import data from "./data";
-import Product from "./components/Product";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            GI-Cart
-          </a>
-        </div>
-        <div className="header-links">
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All Rights Reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              GI-Cart
+            </a>
+          </div>
+          <div className="header-links">
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </main>
+        <footer className="row center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rating from "./Rating";
 
 export default function Product(props) {
   const { product } = props;
@@ -11,7 +12,7 @@ export default function Product(props) {
         <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </a>
-
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <div className="price">${product.price}</div>
       </div>
     </div>
