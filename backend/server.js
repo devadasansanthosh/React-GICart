ce1080a474096f3cb08eca14674eec50377afdb1
Santhosh Devadasan <santhosh.devadasan@in.ibm.com>
2020-12-16 11:50:30 -0600

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import data from "./data.js";
 
 const app = express();
 
@@ -6,6 +7,18 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not found" });
+  }
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               GI-Cart
-            </a>
+            </Link>
           </div>
           <div className="header-links">
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
         </main>
         <footer className="row center">All Rights Reserved</footer>
       </div>
